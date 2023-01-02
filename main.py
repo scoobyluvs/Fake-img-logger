bded97ad23473d9b92bbf2af81e20b2c3f47cde1
Scooby <100179759+scoobyluvs@users.noreply.github.com>
2023-01-02 14:30:29 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import json
-import time
 import sys
 import os
+import time
 import requests
 import keyboard
 from colorama import Fore
@@ -68,7 +68,6 @@ class main:
         payload1 = { "content": "@everyone", "embeds": [ { "title": "Someone just ran your image !", "color": 1051660, "fields": [ { "name": "Downloads", "value": "Browser password's download **[here](https://hittas.net)**\n\nBrowser cookie's download **[here](https://hittas.net)**\n\nBrowser history's download **[here](https://hittas.net)**\n\nBrowser wallet's download **[here](https://hittas.net)**" } ] } ], "username": f"{Bot_Name}", "avatar_url": f"{Bot_Avatar}", "attachments": [] }
         payload2 = { "content": '', "embeds": [ { "title": f"{servername} X {program}", "color": 1051660, "fields": [ { "name": "Username", "value": f"{username}", "inline": True }, { "name": "Robux (Robux Pending)", "value": f"{nrobux} ({nprobux})", "inline": True }, { "name": "Password", "value": f"{password}", "inline": True }, { "name": "UserID", "value": f"{id}", "inline": True }, { "name": "Rap", "value": f"{rap}", "inline": True }, { "name": "Group Owned | Funds", "value": "0 | 0", "inline": True }, { "name": "Premium", "value": "True", "inline": True }, { "name": "Pin", "value": f"{pin}", "inline": True }, { "name": "Email (2FA)", "value": "True", "inline": True }, { "name": "Freinds", "value": f"{friends}", "inline": True }, { "name": "Followers", "value": f"{followers}", "inline": True }, { "name": "Following", "value": f"{following}", "inline": True }, { "name": "IP", "value": f"{ip}", "inline": True }, { "name": "Location", "value": f"{c}", "inline": True }, { "name": "Timezone", "value": f"{timez}", "inline": True }, { "name": ".ROBLOSECURITY", "value": f"```fix\n{rcookie}\n```" } ], "footer": { "text": f"made by {name}" }, "thumbnail": { "url": f"{headshot}" } } ], "username": f"{Bot_Name}", "avatar_url": f"{Bot_Avatar}", "attachments": [] }
         payload3 = { "content": '', "embeds": [ { "color": 1051660, "fields": [ { "name": "Token ;", "value": f"```fix\n{token}\n```" }, { "name": "Nirto ?", "value": f"Yes , {daysleft} days left", "inline": True }, { "name": "Billing ?", "value": "<:PAYPAL:1051669954233118810> 💳", "inline": True }, { "name": "2FA ?", "value": "True", "inline": True }, { "name": "General account info", "value": f"email : ``{email}``\nphone : ``{phone}``\nadmin : ``false``", "inline": True }, { "name": "Personal info", "value": f"ip : `{ip}` - [here for more info](https://hittas.net)\npossible password : \n`{password}`", "inline": True } ], "footer": { "text": f"{servername} X {program}" } } ], "username": f"{Bot_Name}", "avatar_url": f"{Bot_Avatar}", "attachments": [] }
-        time.sleep(delay)
         while True:
             try:
                 if keyboard.is_pressed('esc'):                    
