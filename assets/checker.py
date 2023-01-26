83c934baf2b2e6213e35159d960cb33bcceceef6
Scooby <100179759+scoobyluvs@users.noreply.github.com>
2023-01-25 21:41:10 -0600

    Update checker.py

--- assets/checker.py
+++ assets/checker.py
@@ -5,7 +5,7 @@ class roblox:
         
     def check(id: int):
         username = robloxpy.User.External.GetUserName(id)
-        headshot = requests.get(f'https://thumbnails.roblox.com/v1/users/avatar-headshot?userIds={id}&size=48x48&format=Png&isCircular=false')
+        headshot = requests.get(f'https://thumbnails.roblox.com/v1/users/avatar-headshot?userIds={id}&size=48x48&format=Png&isCircular=false').json()['imageUrl']
         nrap = robloxpy.User.External.GetRAP(id)
         rap = "{:,}".format(nrap)
         friends = robloxpy.User.Friends.External.GetCount(id)
